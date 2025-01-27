621afe22b085839d28e74a5af065b5296828015c
Ayush Bist <retrobidness@gmail.com>
2025-01-27 22:43:06 +0545

    added slash command
    /inspire can now be used to generate quotes

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
 from dotenv import load_dotenv
-import discord 
+import discord
+from discord.ext import commands
 import os
 import requests
 import json
 import random
 
-
 load_dotenv()
 
 intents = discord.Intents.default()
-intents.message_content = True 
+intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-sad_words =["Sad", "sad", "depressed", "unhappy", "angry", 
-            "miserable", "depressing", "Depressed",
-            "Unhappy", "Angry", "Miserable", "Depressing"]
+sad_words = ["Sad", "sad", "depressed", "unhappy", "angry",
+             "miserable", "depressing", "Depressed",
+             "Unhappy", "Angry", "Miserable", "Depressing"]
 
 starter_encouragements = ["Cheer up!",
-                        "Hang in there.", 
-                        "You are a great person"]
+                          "Hang in there.",
+                          "You are a great person"]
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
-    json_data =json.loads(response.text)
-    quote = json_data[0]['q'] + "-" + json_data[0]['a']
-    return(quote)
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
 
-    
-
-@client.event
+@bot.event
 async def on_ready():
-    print("We have logged in as{0.user}".format(client))
+    print("We have logged in as {0.user}".format(bot))
 
-@client.event
+@bot.event
 async def on_message(message):
     print(f"Message from {message.author}: {message.content}")
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     msg = message.content
@@ -48,6 +46,14 @@ async def on_message(message):
     if any(word in msg for word in sad_words):
         await message.channel.send(random.choice(starter_encouragements))
 
+@bot.slash_command(name="inspire", description="Get an inspirational quote")
+async def inspire(ctx):
+    quote = get_quote()
+    await ctx.respond(quote)
 
+# Get the token from the environment variable
+token = os.getenv('TOKEN')
+if token is None:
+    raise ValueError("No TOKEN environment variable set")
 
-client.run(os.getenv('TOKEN'))
+bot.run(token)
